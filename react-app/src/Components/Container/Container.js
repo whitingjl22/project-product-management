98cda76b5205c6bc8dd9409d4f980d9b0d12f834
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-29 10:40:28 -0500

    clean-up

---
+++ react-app/src/Components/Container/Container.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h1>CONTAINER PAGE</h1>
+      </div>
+    )
+  }
+}
+
+export default Container
